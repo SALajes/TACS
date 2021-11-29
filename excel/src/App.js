26bdaf421a3d1d2d21d47572589329b77f37ca96
AndreiaGouveia <andreiagouveia1@gmai.com>
2021-11-29 15:33:30 +0000

    Initial Setup

---
+++ excel/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import DataGridDemo from './components/DataGridDemo.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <DataGridDemo/>
+    </div>
+  );
+}
+
+export default App;
